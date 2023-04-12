a489e03b6462cc6f3e7ebc5f71e5a4671bd415ca
tobi <tobibags19@gmail.com>
2023-04-12 08:26:51 -0500

    Send an email with new titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "d8944af9de5c45b99ab0641a4b7217d6"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
@@ -12,6 +13,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "tobi19.py@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    receiver = "tobi19.py@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
